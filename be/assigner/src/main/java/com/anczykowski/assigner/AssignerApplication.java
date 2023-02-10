fcb13edd6234aa72c5b85377bf90169548e7222f
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2023-02-10 12:57:17 +0100

    Initial BE setup (spring-initializer)

---
+++ be/assigner/src/main/java/com/anczykowski/assigner/AssignerApplication.java
@@ -0,0 +1,13 @@
+package com.anczykowski.assigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignerApplication.class, args);
+	}
+
+}
