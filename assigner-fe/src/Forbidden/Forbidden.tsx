8871d30eb337a8df0d10e25afefaf3eba3a24aba
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2023-07-14 08:33:20 +0200

    403 page

---
+++ assigner-fe/src/Forbidden/Forbidden.tsx
@@ -0,0 +1,13 @@
+import './Forbidden.css';
+
+function Forbidden() {
+  return (
+    <div className='Assigner-center-container'>
+      <header className='Assigner-center Assigner-header'>
+        <p>Brak uprawnień do zasobu. (403)</p>
+      </header>
+    </div>
+  );
+}
+
+export default Forbidden;
