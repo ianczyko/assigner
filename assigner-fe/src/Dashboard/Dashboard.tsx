fad22b456d06763a0c52fa7b50e37e6cbd364656
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2023-11-08 22:07:39 +0100

    logout button

--- assigner-fe/src/Dashboard/Dashboard.tsx
+++ assigner-fe/src/Dashboard/Dashboard.tsx
@@ -4,6 +4,7 @@ import wretch from 'wretch';
 import './Dashboard.css';
 import Helpers from '../Common/Helpers';
 import { ToastContainer } from 'react-toastify';
+import { Button, Stack } from '@mui/material';
 
 function Dashboard() {
   interface IProfileResponse {
@@ -31,17 +32,37 @@ function Dashboard() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  function handleLogout() {
+    wretch('/api/logout')
+      .post()
+      .unauthorized((error) => {
+        Helpers.handleUnathorised(navigate);
+      })
+      .forbidden((error) => {
+        Helpers.handleForbidden();
+      })
+      .res(() => {
+        navigate('/');
+      })
+      .catch((error) => console.log(error));
+  }
+
   if (profile != null) {
     return (
       <div className='Assigner-center-container'>
         <header className='Assigner-center Assigner-header'>
           <ToastContainer />
-          <p>
-            {profile.id} - {profile.first_name} - {profile.last_name}
-          </p>
-          <Link className='Assigner-link' to='/courses'>
-            Kursy
-          </Link>
+          <Stack spacing='20px'>
+            <p>
+              {profile.id} - {profile.first_name} - {profile.last_name}
+            </p>
+            <Button variant='contained' onClick={handleLogout}>
+              Wyloguj
+            </Button>
+            <Link className='Assigner-link' to='/courses'>
+              Kursy
+            </Link>
+          </Stack>
         </header>
       </div>
     );
