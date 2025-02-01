212ac0a4c89b06ad6d87a688f028ded00ecd1474
Igor Anczykowski <i.anczykowski@stud.elka.pw.edu.pl>
2025-02-01 13:54:42 +0100

    Open source solver - JSR331 + GLPK

--- assigner/src/main/java/com/anczykowski/assigner/solver/services/SolverService.java
+++ assigner/src/main/java/com/anczykowski/assigner/solver/services/SolverService.java
@@ -4,12 +4,14 @@ import com.anczykowski.assigner.projects.ProjectsService;
 import com.anczykowski.assigner.solver.models.AssignOptimizationResult;
 import com.anczykowski.assigner.teams.TeamsRepository;
 import com.anczykowski.assigner.teams.TeamsService;
-import ilog.concert.IloException;
-import ilog.concert.IloNumVar;
-import ilog.cplex.IloCplex;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.constraints.Objective;
+import javax.constraints.ProblemFactory;
+import javax.constraints.VarReal;
+import java.util.ArrayList;
 
 @Service
 @Transactional(readOnly = true)
@@ -31,71 +33,72 @@ public class SolverService {
 
         var projects = projectsService.getProjects(courseName, edition, groupName);
 
-        try (IloCplex cplex = new IloCplex()) {
-            cplex.setOut(null);
+        var problem = ProblemFactory.newProblem("AssignProjects");
 
-            //// parameters
-            int T = teams.size();
-            var P = projects.size();
+        //// parameters
+        var T = teams.size();
+        var P = projects.size();
 
-            //// variables
-
-            var teams_project_assignment = new IloNumVar[T][];
-            for (int i = 0; i < T; ++i) {
-                teams_project_assignment[i] = cplex.numVarArray(P, 0.0, 1.0);
+        //// variables
+        var teamProjectAssignment = new VarReal[T][P];
+        for (var t = 0; t < T; t++) {
+            for (var p = 0; p < P; p++) {
+                teamProjectAssignment[t][p] = problem.variableReal("team_%d_project_%d".formatted(t, p), 0.0, 1.0);
             }
+        }
 
-            //// constraints
-            // one_project_per_team
-            for (int i = 0; i < T; ++i) {
-                cplex.addEq(cplex.sum(teams_project_assignment[i]), 1);
+        //// constraints
+        // teams_project_assignment
+        for (var t = 0; t < T; t++) {
+            problem.post(problem.sum(teamProjectAssignment[t]), "=", 1.0);
+        }
+
+        // project_team_limits_constraint
+        for (var p = 0; p < P; p++) {
+            var column = new VarReal[T];
+            for (var t = 0; t < T; t++) {
+                column[t] = teamProjectAssignment[t][p];
             }
+            var project = projects.get(p);
+            problem.post(problem.sum(column), "<=", project.getEffectiveLimit());
+        }
 
-            // project_team_limits_constraint
+        //// objective
+        var satisfaction = new ArrayList<VarReal>(T * P);
+        for (int i = 0; i < T; ++i) {
             for (int j = 0; j < P; ++j) {
-                var column = cplex.linearNumExpr();
-                for (int i = 0; i < T; ++i) {
-                    column.addTerm(1, teams_project_assignment[i][j]);
-                }
+                var team = teams.get(i);
                 var project = projects.get(j);
-                cplex.addLe(column, project.getEffectiveLimit());
+                var rating = team.getPreferenceFor(project);
+                satisfaction.add(teamProjectAssignment[i][j].multiply(rating));
             }
+        }
+        var satArr = satisfaction.toArray(new VarReal[0]);
+        var objective = problem.sum(satArr);
+        objective.setName("objective");
 
-            //// objective
-            var satisfaction = cplex.linearNumExpr();
-            for (int i = 0; i < T; ++i) {
-                for (int j = 0; j < P; ++j) {
-                    var team = teams.get(i);
-                    var project = projects.get(j);
-                    var rating = team.getPreferenceFor(project);
-                    satisfaction.addTerm(teams_project_assignment[i][j], rating);
-                }
-            }
+        var solver = problem.getSolver();
+        var solution = solver.findOptimalSolution(Objective.MAXIMIZE, objective);
 
-            cplex.addMaximize(satisfaction);
-
-            if (cplex.solve()) {
-                var assignOptimizationResult = new AssignOptimizationResult();
-                assignOptimizationResult.setObjective(cplex.getObjValue());
-                for (int i = 0; i < T; ++i) {
-                    for (int j = 0; j < P; ++j) {
-                        var floatAssignment = cplex.getValue(teams_project_assignment[i][j]);
-                        var isAssigned = Math.abs(floatAssignment - 1.0) < EPSILON;
-                        if (isAssigned) {
-                            var team = teams.get(i);
-                            var project = projects.get(j);
-                            team.setAssignedProject(project);
-                            teamsRepository.save(team);
-                        }
+        if (solution != null) {
+            var assignOptimizationResult = new AssignOptimizationResult();
+            assignOptimizationResult.setObjective(solution.getValueReal("objective"));
+            for (var t = 0; t < T; ++t) {
+                for (var p = 0; p < P; ++p) {
+                    var floatAssignment = solution.getValueReal(teamProjectAssignment[t][p].getName());
+                    var isAssigned = Math.abs(floatAssignment - 1.0) < EPSILON;
+                    if (isAssigned) {
+                        var team = teams.get(t);
+                        var project = projects.get(p);
+                        team.setAssignedProject(project);
+                        teamsRepository.save(team);
                     }
                 }
-                assignOptimizationResult.setTeams(teams);
-                return assignOptimizationResult;
             }
-        } catch (IloException e) {
-            throw new RuntimeException(e);
+            assignOptimizationResult.setTeams(teams);
+            return assignOptimizationResult;
+
         }
         return null;
     }
-
 }
